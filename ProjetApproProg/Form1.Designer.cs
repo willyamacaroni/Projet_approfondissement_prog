558ce6a290f6826007b3b97a717eb089abbaa686
Cédric Bouchard <cedricbouchard170@gmail.com>
2020-10-01 15:55:20 -0400

    Mise en page de base

---
+++ ProjetApproProg/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace ProjetApproProg
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtRecherche = new System.Windows.Forms.TextBox();
+            this.btnRecherche = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtRecherche
+            // 
+            this.txtRecherche.BackColor = System.Drawing.Color.White;
+            this.txtRecherche.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtRecherche.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtRecherche.ForeColor = System.Drawing.SystemColors.ScrollBar;
+            this.txtRecherche.Location = new System.Drawing.Point(170, 337);
+            this.txtRecherche.Name = "txtRecherche";
+            this.txtRecherche.Size = new System.Drawing.Size(694, 46);
+            this.txtRecherche.TabIndex = 0;
+            this.txtRecherche.Text = "Rechercher...";
+            this.txtRecherche.Enter += new System.EventHandler(this.textBox1_Enter);
+            // 
+            // btnRecherche
+            // 
+            this.btnRecherche.BackColor = System.Drawing.Color.White;
+            this.btnRecherche.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnRecherche.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnRecherche.Location = new System.Drawing.Point(870, 337);
+            this.btnRecherche.Name = "btnRecherche";
+            this.btnRecherche.Size = new System.Drawing.Size(46, 46);
+            this.btnRecherche.TabIndex = 1;
+            this.btnRecherche.UseVisualStyleBackColor = false;
+            this.btnRecherche.Click += new System.EventHandler(this.btnRecherche_Click);
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.ClientSize = new System.Drawing.Size(1062, 673);
+            this.Controls.Add(this.btnRecherche);
+            this.Controls.Add(this.txtRecherche);
+            this.Name = "frmPrincipal";
+            this.Text = "Multi Recherche";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtRecherche;
+        private System.Windows.Forms.Button btnRecherche;
+    }
+}
+
---
+++ ProjetApproProg/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjetApproProg
+{
+    public partial class frmPrincipal : Form
+    {
+        public frmPrincipal()
+        {
+            InitializeComponent();
+            this.BackColor = Color.FromArgb(25, 20, 30);
+        }
+
+        private void textBox1_Enter(object sender, EventArgs e)
+        {
+            this.txtRecherche.Text = String.Empty;
+            this.txtRecherche.ForeColor = Color.Black;
+        }
+
+        private void btnRecherche_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
